26c9675485f1ee1571b810d5d370d691ea114143
antl-mark <a.markevuch@ukr.net>
2017-06-07 07:20:13 +0300

    correcting grammatical errors

--- randome_numbers.rb
+++ randome_numbers.rb
@@ -3,13 +3,13 @@ RANGE_NAMBER = 1000..9999
 RANGE_LITTERS = ('a'..'z').to_a
 
 def generate
-  puts "#{rand_numbers}-#{rand_letters}-#{rand_numbers}"
+  "#{numbers}-#{letters}-#{numbers}"
 end
 
-def rand_numbers
+def numbers
   rand(RANGE_NAMBER)
 end
 
-def rand_letters
+def letters
   RANGE_LITTERS.sample(4).join.upcase
 end
